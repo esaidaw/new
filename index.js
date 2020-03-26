1936521b995e2170876470593d93091570bf2bb6
esaidaw <60747341+esaidaw@users.noreply.github.com>
2020-03-26 23:21:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+$(".i").on("click",function(){
+  $(".i").hide();
+  $(".i").after("<h1> You embraced so many beautiful memories </h1>")
+})
+$(".k").on("click",function(){
+  $(".k").hide();
+  $(".k").after("<h1> All I want is you </h1>")
+})
+$(".l").on("click",function(){
+  $(".l").hide();
+  $(".l").after("<h1> Im alive when im with you </h1>")
+})
+$(".m").on("click",function(){
+  $(".m").hide();
+  $(".m").after("<h1> I see my future in you  </h1>")
+})
+$(".n").on("click",function(){
+  $(".n").hide();
+  $(".n").after("<h1> You're fucking delicious,be my forever snack</h1>")
+})
